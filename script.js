d94e7bd69e2bedf1af89bab6df1b7ac4ec9df2fc
scrif <andrews.austin.94@gmail.com>
2022-05-02 11:14:26 -0500

    Add default grid size, event listeners

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector('.container');
 const clearButton = document.querySelector('.clear');
 
+// On Page Load - default size
+createGrid(10);
 
 function createGrid() {
     let gridArea = gridNumber * gridNumber;
@@ -18,3 +20,14 @@ function wipeColors() {
     var gridPixels = container.querySelectorAll('div');
     gridPixels.forEach(gridPixel => gridPixel.style.backgroundColor = '#ffffff');
 }
+
+// Event Listeners
+clearButton.addEventListener('click', eraseAllColor);
+clearButton.addEventListener('mouseover', buttonHover);
+clearButton.addEventListener('mouseout', buttonStandard);
+colorButtons.forEach(colorButton => colorButton.addEventListener('click', changeColor));
+colorButtons.forEach(colorButton => colorButton.addEventListener('mouseover', buttonHover));
+colorButtons.forEach(colorButton => colorButton.addEventListener('mouseout', buttonStandard));
+slider.addEventListener('mouseup', pixelSize);
+userColorPicker.addEventListener('change', userColorSelection, false);
+userColorPicker.addEventListener('input', userColorSelection, false);
