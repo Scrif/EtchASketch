d785239bc26341de944f2baeabc3e684e65a2ba1
scrif <andrews.austin.94@gmail.com>
2022-05-01 22:02:23 -0500

    Add createGrid() functions

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function createGrid() {
-    
+    let area = gridNumber * gridNumber;
+    for (let i = 1, i <= area; i++) {
+        let gridItem = document.createElement('div');
+        container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
+        container.insertAdjacentElement('beforeend', gridItem);
+    }
+    var gridPixels = container.querySelectorAll('div');
+    gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', colorGrid));
 }
