08784f1db77608cf456e9421457030ee935467d2
scrif <andrews.austin.94@gmail.com>
2022-05-09 15:59:26 -0500

    Add function declarations, change variable declarations a bit

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const defaultColor = '#ffffff'
-const defaultSize = 16
 const grid = document.querySelector('.grid')
 
 let backgroundColor = defaultColor
-let gridDim = defaultSize
+let gridDim = document.querySelector('.grid-dim')
+let 
 
+
+// Function Declarations below:
 function createGrid() {
-    grid.style.gridDefaultColumns
-    grid.style.gridDefaultRows
+
 }
 
-createGrid()
+createGrid(blockSize) {
+
+}
+
+setBlockSize(grid)
