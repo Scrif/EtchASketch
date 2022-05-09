447d17e900c33a0da7fedaf975918155a35ad60d
scrif <andrews.austin.94@gmail.com>
2022-05-09 16:43:11 -0500

    Add reset() function, createGrid function

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ const defaultColor = '#ffffff'
 const grid = document.querySelector('.grid')
 
 let backgroundColor = defaultColor
-let gridDim = document.querySelector('.grid-dim')
-let 
+let gridSize = document.querySelector('.grid-size')
+let blockSize = 16
 
 
 // Function Declarations below:
-function createGrid(gridDim) {
 
+// This function creates the grid of blocks
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            grid.appendChild(createDiv(grid.clientWidth / gridSize));
+        }
+    }
 }
 
 // This function sets the block size and creates the individual divs
@@ -18,4 +24,11 @@ function createBlock(size) {
     div.style.height = '${size}px';
     div.style.width = '${size}px';
     return block;
+}
+
+function reset() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+    }
+    createGrid(blockSize);
 }
